d111caa7ae2317e2da49cf3564cf30855e708791
Junseok <oh971021@gmail.com>
2021-09-11 14:32:00 +0000

    0911 java study : Print

---
+++ 04_Print/src/PMain_1.java
@@ -0,0 +1,13 @@
+public class PMain_1 {
+	public static void main(String[] args) {
+		String str1 = "Hello";
+		String str2 = "World";
+		int a = 1010;
+		int b = 2020;
+		
+		System.out.print(str1);
+		System.out.println(str2);
+		System.out.printf("%d, %d", a, b);
+		
+	}
+}
