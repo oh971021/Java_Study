43f60368c82357630b68375a166d8a0737267a72
Junseok <oh971021@gmail.com>
2021-09-15 22:09:08 +0900

    0915 java study : Useful Class

--- 10_UsefulClass/src/UMain_2.java
+++ 10_UsefulClass/src/UMain_2.java
@@ -1,3 +1,5 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class UMain_2 {
@@ -14,11 +16,35 @@ public class UMain_2 {
 		
 		*/
 		
-		// 시간
-		System.out.println(d.getHours());
-		// 분
+		// 월 일 시 분 
+		SimpleDateFormat sdf = new SimpleDateFormat("MMMdd일 hh시 mm분");
+		System.out.println(sdf.format(d));
 		
-		// 초
+		String string = sdf.format(d);
+		System.out.println(string);
+		
+		System.out.println("============================");
+		
+		// Test
+		
+		// 1. sdf2는 시:분:초
+		SimpleDateFormat sdf2 = new SimpleDateFormat("hh:mm:ss");
+		System.out.println(sdf2.format(d));
+		
+		// String -> date 타입으로 변경
+		String ds = "94-03-02";
+		SimpleDateFormat sdf3 = new SimpleDateFormat("yy-MM-dd");
+		
+		// String에서 Date로 변환할 때, 예외처리를 사용한다.
+		try {
+			Date d2 = sdf3.parse(ds);
+			System.out.println(d2);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 		
 		
