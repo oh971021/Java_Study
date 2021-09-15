ff647c66edc4fabfc83c00352236205e9b83305f
Junseok <oh971021@gmail.com>
2021-09-15 22:09:34 +0900

    0915 java study : OOP

---
+++ 11_OOPBasic/src/Dog.java
@@ -0,0 +1,27 @@
+
+// °­¾ÆÁö °´Ã¼¸¦ ¸¸µç´Ù.
+public class Dog {
+	
+	// field ¼±¾ð
+	String name;	// member º¯¼ö
+	int age;		 
+	double weight;  
+	
+	// ±â´É
+	void Bark() {
+		System.out.println("¸Û¸Û");
+	}
+	
+	void PrintInfo(String name, int age, double weight) {
+		System.out.println("=========ÀÎ½ÄÇ¥=========");
+		System.out.println("ÀÌ¸§:"+name+"  ³ªÀÌ:"+age+"  Ã¼Áß:"+weight);
+	}
+	
+	void Sit(String a) {
+		if (a.equals("¾É¾Æ")) {
+			System.out.println("ÅÐ½â");
+		} else {
+			System.out.println("¿Õ¿Õ!");
+		}
+	}
+}
