17826e56289ee013ac4a3955adea277db61726ca
Junseok <oh971021@gmail.com>
2021-09-16 15:36:35 +0900

    0916 javastudy : OOP

--- 11_OOPBasic/src/Practice02.java
+++ 11_OOPBasic/src/Practice02.java
@@ -6,14 +6,20 @@ public class Practice02 {
 		dog.setKinds("강아지");
 		dog.setAge(8);
 		
+		Animal02 dog2 = new Animal02();
+		dog2.setKinds("멍멍이");
+		dog2.setAge(7);
+		
 		Animal02 cat = new Animal02();
 		cat.setKinds("고양이");
 		cat.setAge(13);
 		
 		dog.Move();
+		dog2.Move();
 		cat.Move();
 		
 		System.out.printf("%s는 %d개월 입니다.\n", dog.getKinds(), dog.getAge());
+		System.out.printf("%s는 %d개월 입니다.\n", dog2.getKinds(), dog2.getAge());
 		System.out.printf("%s는 %d개월 입니다.\n", cat.getKinds(), cat.getAge());
 	}
 }
@@ -39,6 +45,10 @@ class Animal02 {
 	}
 
 	public void setAge(int age) {
-		this.age = age;
+		if (age > 100) {
+			this.age = 100;	
+		} else {
+			this.age = age;
+		}
 	}
 }
