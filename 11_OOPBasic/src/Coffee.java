5a8d104c47c3df0c1befece4727d1e151e4fd4c0
Junseok <oh971021@gmail.com>
2021-09-17 23:58:43 +0900

    0917 java study : OOP

--- 11_OOPBasic/src/Coffee.java
+++
@@ -1,9 +0,0 @@
-public class Coffee {
-	int price;
-	String kinds;
-	
-	public void coffeeInto() {
-		System.out.println("주문하신 "+kinds+" 나왔습니다.");
-		System.out.println("가격은 "+price+" 원 입니다.");
-	}
-}
--- 11_OOPBasic/src/Product.java
+++
@@ -1,9 +0,0 @@
-public class Product {
-	String item;
-	int price;
-	String origin;
-	
-	public void printInfo() {
-		System.out.println("풍명이 "+item+", 가격이 "+price+", 원산지가 "+origin);
-	}
-}
