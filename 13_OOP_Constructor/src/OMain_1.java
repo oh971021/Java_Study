ca30a395ba44e61bf690bb5e9dddcffde7977722
Junseok <oh971021@gmail.com>
2021-09-17 11:44:23 +0900

    0917 java study : OOP_Constructor

---
+++ 13_OOP_Constructor/src/OMain_1.java
@@ -0,0 +1,13 @@
+public class OMain_1 {
+	public static void main(String[] args) {
+		
+		// 사람
+		// 이름, 성별
+		
+		// 새로운 사람이 태어날 때 "응애" 출력
+		Person a = new Person("준석", "남");
+		Person b = new Person("솔리", "여");
+
+		
+	}
+}
