7a1acb5f8173dbbd088f83ccfe5b052e5b3a6d66
Junseok <oh971021@gmail.com>
2021-09-23 11:25:57 +0900

    0921 java Variable review

---
+++ 01_Variable/src/VMain_1.java
@@ -0,0 +1,18 @@
+public class VMain_1 {
+	public static void main(String[] args) {
+		int a;
+		double b;
+		String c;
+		float d;
+		
+		a = 5;
+		b = 5.5;
+		c = "¡ÿºÆ";
+		d = 4.4f;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
