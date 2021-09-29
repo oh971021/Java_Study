03a833b75844d93fd6db46b06eb27fb594c1ad5a
Junseok <oh971021@gmail.com>
2021-09-29 10:15:42 +0900

    0929 java study : Singleton

---
+++ 23_Singletonpattern/src/SPMain_1.java
@@ -0,0 +1,16 @@
+
+public class SPMain_1 {
+	public static void main(String[] args) {
+		
+		// 자기자신 : 이름, 나이
+		Me m = new Me("준석", 25);
+		m.info();
+		System.out.println(m);
+		
+		Me m2 = new Me("준석", 25);
+		m2.info();
+		System.out.println(m2);
+		
+		
+	}
+}
